ca38b06508f765f849a75563e22ee55fb8f04d92
pmaaa2-iscte <pmaaa2@iscte-iul.pt>
2023-02-16 12:09:42 +0000

    Completed Foo class

--- src/Foo.java
+++ src/Foo.java
@@ -1,4 +1,18 @@
 
 public class Foo {
-
+	
+	private int total;
+	
+	public Foo(int a, int b) {
+		total = a + b;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	
+	public static void main(String[] args) {
+		Foo f = new Foo(10, 20);
+		System.out.println("The total is: " + f.getTotal());
+	}
 }
