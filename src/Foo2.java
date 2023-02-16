f6f304e0c9ea286a7b5f2e525cc64220948d41bc
pmaaa2-iscte <pmaaa2@iscte-iul.pt>
2023-02-16 12:27:35 +0000

    Small changes to Foo2

--- src/Foo2.java
+++ src/Foo2.java
@@ -1,4 +1,18 @@
 
 public class Foo2 {
-
+	
+	private int total;
+	
+	public Foo2(int a, int b) {
+		total = a - b;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	
+	public static void main(String[] args) {
+		Foo2 f = new Foo2(20, 10);
+		System.out.println("The total is: " + f.getTotal());
+	}
 }
